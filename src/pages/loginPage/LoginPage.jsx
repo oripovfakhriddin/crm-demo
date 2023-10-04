72aa36978e1c424a5ef276950352f693629145a9
oripovfakhriddin <fakhriddinoripov0731@gmail.com>
2023-10-04 16:51:37 +0500

    Start

--- src/pages/loginPage/LoginPage.jsx
+++ src/pages/loginPage/LoginPage.jsx
@@ -4,13 +4,17 @@ import { useNavigate } from 'react-router-dom'
 
 const LoginPage = () => {
 
-  const [validated, setValidated] = useState();
+  const [validated, setValidated] = useState(false);
   const navigate = useNavigate()
 
   const handleSubmit = (e) =>{
-    const form  = e.terget
+    const form  = e.currentTarget;
     e.preventDefault();
-    navigate("/product")
+    if (form.checkValidity()) {
+      navigate("/product")
+    } else {
+      setValidated(true)
+    }
   }
 
 
@@ -19,7 +23,7 @@ const LoginPage = () => {
     <Fragment>
       <Container>
         <div className='vh-100 d-flex align-items-center justify-content-center'>
-          <Form  noValidate validated={validated} onSubmit={handleSubmit} >
+          <Form  noValidate validated={validated}  onSubmit={handleSubmit} >
             <Form.Group className='mb-4' md="6"  controlId="firstName">
               <Form.Label>Username</Form.Label>
               <Form.Control
@@ -33,6 +37,7 @@ const LoginPage = () => {
               <Form.Label>Password</Form.Label>
               <Form.Control
                 required
+                placeholder='Password'
                 type="password" />
               <Form.Control.Feedback type='invalid'>Please fill!</Form.Control.Feedback>
             </Form.Group>
